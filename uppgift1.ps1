f32269a13e6f0299a75eaac30046aff8acad2ca7
Lukas Bye <lukas.bye@elev.ga.ntig.se>
2023-09-28 13:31:43 +0200

    ip loop

--- uppgift1.ps1
+++ uppgift1.ps1
@@ -1,5 +1,8 @@
-$first = @(192.168.0.)
+
 
 for ($i = 1; $i -lt 256; $i++) {
-    Write-Output "$first$i";
+    $ping = Test-Connection "10.80.44.$i" -quiet -count 1;
+    if ($ping) {
+        Write-Output "10.80.44.$i"
+    }
 }
